54edd344eedac6e8b64646cf71fb8022a84bc647
Krooper <98321262+Krooper@users.noreply.github.com>
2023-02-10 21:15:08 +0300

    made 1st HW

---
+++ 1/Beverages and VendingMachines/src/HotBevVendingMachine.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HotBevVendingMachine extends BevVendingMachine {
+
+    private ArrayList<Product> hotBevs = new ArrayList<>();
+
+    public void initProducts(ArrayList<Product> someBevs) {
+        for (Product someBev : someBevs) {
+            hotBevs.add(someBev);
+        }
+    }
+
+    @Override
+    public String getProduct(String name) {
+        return "We have different options for " + name + ". Please choose precisely!";
+    }
+
+    @Override
+    public String getProduct(String name, int volume) {
+        return "We have different options for " + name + " of volume " + volume + ". Please choose precisely!";
+    }
+
+    @Override
+    public String getProduct(String name, int volume, int temp) {
+        return "Here's your " + name + " of volume " +
+                volume + " ml, its temperature - " + temp + " Celsius.";
+    }
+}
