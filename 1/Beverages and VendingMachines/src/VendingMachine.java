1335baca55f7befe9f025a19999d1a2cc8ace246
Krooper <98321262+Krooper@users.noreply.github.com>
2023-02-10 21:12:25 +0300

    made 1st HW

---
+++ 1/Beverages and VendingMachines/src/VendingMachine.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public interface VendingMachine {
+
+    void initProducts(ArrayList<Product> someProducts);
+
+    String getProduct(String name);
+
+    String getProduct(String name, int volume);
+
+    String getProduct(String name, int volume, int temp);
+}
